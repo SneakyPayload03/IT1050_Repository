a73c10091d28732c576e508e98018a43dd687b83
Ariel Clark <S01227048@acad.tri-c.edu>
2019-09-17 19:41:47 -0400

    Art

---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I hope this fucking works");
+            Console.WriteLine("Enter to continue.....");
+            Console.ReadLine();
+            Console.WriteLine("Hello My Name Is Supreme Overlord!");
+            Console.ReadLine();
+            Console.WriteLine("LOL jk!");
+            Console.WriteLine("My name is actually Ariel.");
+            Console.ReadLine();
+            Console.WriteLine("But in all seriousness, prepare yourself because i will unite all nations under my supreme rule!");
+            Console.ReadLine();
+            Console.WriteLine("Mwhahahahahahahahahahahahahahahahahahahahahahahahahaha");
+            Console.ReadLine();
+            Console.WriteLine("Mwhahahahahahahahahahahahahahahahahahahahahahahahahaha");
+            Console.ReadLine();
+            Console.WriteLine("Mwhahahahahahahahahahahahahahahahahahahahahahahahahaha");
+            Console.ReadLine();
+            Console.WriteLine("Mwhahahahahahahahahahahahahahahahahahahahahahahahahaha");
+
+        }
+    }
+}
