6e2c7b337f8550fa5484d98f4d23a2c2d0b013a8
Ariel Clark <S01227048@acad.tri-c.edu>
2019-09-19 10:23:21 -0400

    Beginnning of program :)

---
+++ Voter Elligibility Check/Voter Elligibility Check/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voter_Elligibility_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            string firstName;
+            char middleInitial;
+            string lastName;
+            string fullName;
+            int age;
+            bool isCitizen;
+            bool canVote;
+            int heightFeet;
+            double heightInches;
+            double totalHeightCM;
+
+
+            Console.WriteLine("Official OHIO Voter Eligibility and Citizen Verification Software");
+            Console.WriteLine("===================================================================");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+            Console.WriteLine("To get started you need to provide some personal information");
+
+
+                
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
