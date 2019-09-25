9992aa844171e57475167dd315d8eb0c19bc52ec
Ariel Clark <S01227048@acad.tri-c.edu>
2019-09-24 20:40:20 -0400

    I DID IT

--- Voter Elligibility Check/Voter Elligibility Check/Program.cs
+++ Voter Elligibility Check/Voter Elligibility Check/Program.cs
@@ -10,15 +10,15 @@ namespace Voter_Elligibility_Check
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+
+            const double multiplier = 2.54;
+
             string firstName;
-            char middleInitial;
+            string middleName;
             string lastName;
             string fullName;
             int age;
             bool isCitizen;
-            bool canVote;
             int heightFeet;
             double heightInches;
             double totalHeightCM;
@@ -30,8 +30,39 @@ namespace Voter_Elligibility_Check
             Console.WriteLine();
             Console.ReadLine();
             Console.WriteLine("To get started you need to provide some personal information");
+            Console.WriteLine("Please Enter Your First Name Below.");
+            firstName = Console.ReadLine();
+            Console.WriteLine("What is your Middle Name?");
+            middleName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            lastName = Console.ReadLine();
+            fullName = firstName + " " + middleName + " " + lastName;
+            Console.WriteLine();
+
+            Console.WriteLine(fullName + " how tall are you in feet?");
+            heightFeet = int.Parse(Console.ReadLine());
+            
+            
+            int inches = heightFeet * 12;
+            double heightCM = multiplier * inches;
+
+            Console.WriteLine(fullName + " you are " + heightCM + " CMs tall.");
+            Console.ReadKey();
+            Console.WriteLine("Are you a citizen?");
+            isCitizen = bool.Parse(Console.ReadLine());
+            
+            Console.WriteLine(fullName + " how old are you?");
+            age = int.Parse(Console.ReadLine());
+            bool canVote = age >=18 && isCitizen;
 
 
+
+            Console.WriteLine("VOTER ELIGIBILITY:");
+            Console.WriteLine(canVote);
+
+            Console.ReadKey();
+
+         
                 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
