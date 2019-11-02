56b4d9755c88d1c7e08be45ffe3219bdeeea3389
Kathryn Clark <S01227048@acad.tri-c.edu>
2019-11-02 08:40:50 -0400

    Made Instructor Class

--- BetterBlackboard/BetterBlackboard/Instructor.cs
+++ BetterBlackboard/BetterBlackboard/Instructor.cs
@@ -8,5 +8,32 @@ namespace BetterBlackboard
 {
     class Instructor
     {
+        private string FirstName = "Donnie";
+        private string LastName= "Santos";
+        private string CourseName = "IT-1050";
+        Student Ariel = new Student();
+        public string SetInstructorName(string First, string Last)
+        {
+            this.FirstName = First;
+            this.LastName = Last;
+            return this.FirstName + " " + this.LastName;
+        }
+        public string SetInstructorCourses(string course)
+        {
+            this.CourseName = course;
+            return CourseName;
+        }
+        public void SetStudentGrade(Student StudentName, int Grade)
+        {
+            Grade = StudentName.SetGrade();
+        }
+        public void GetInstructorInfo(Student StudentName)
+        {
+            Console.WriteLine(this.SetInstructorName(this.FirstName, this.LastName));
+            Console.WriteLine(SetInstructorCourses(this.CourseName));
+            StudentName.GetStudentName();
+            
+            
+        }
     }
 }
--- BetterBlackboard/BetterBlackboard/Student.cs
+++ BetterBlackboard/BetterBlackboard/Student.cs
@@ -8,5 +8,28 @@ namespace BetterBlackboard
 {
     class Student
     {
+        private string FirstName = "John";
+        private string LastName = "Doe";
+        private int Grade = 0;
+
+        private Instructor Donnie = new Instructor;
+        public int SetGrade()
+        {
+            return this.Grade;
+        }
+        public string SetStudentName(string First, string Last)
+        {
+            this.FirstName = First;
+            this.LastName = Last;
+            return this.FirstName + " " + this.LastName;
+        }
+        public void GetStudentName()
+        {
+            Console.WriteLine(this.FirstName + " " + this.LastName);
+        }
+
+
+
+
     }
 }
