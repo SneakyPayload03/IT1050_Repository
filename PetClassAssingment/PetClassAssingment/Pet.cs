14e31b201ca972e0acff1397c2c7effd5253478e
Ariel Clark <S01227048@acad.tri-c.edu>
2019-12-03 20:03:53 -0500

    :o

---
+++ PetClassAssingment/PetClassAssingment/Pet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetClassAssingment
+{
+    class Pet
+    {
+        private string Name = "Happy";
+        private int Age = 6;
+        private string Breed = "Flying Cat";
+        Public Pet(string name, int age, string breed)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Breed = breed;
+            
+        }
+        public void printAll()
+        {
+
+        }
+    }
+}
---
+++ PetClassAssingment/PetClassAssingment/PetOwner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetClassAssingment
+{
+    class PetOwner
+    {
+        private string Name = "Natsu";
+        private int Age = 400;
+        Pet Animal;
+        
+        public PetOwner(string name, int age, Pet animal, int numberofpets, Pet petName)
+        {
+            getName(name);
+            getAge(age);
+            new animal = Animal Pet()
+        }
+        public void getName(string name)
+        {
+            this.Name = name;
+        }
+        public void getAge(int age)
+        {
+            this.Age = age;
+        }
+        public void printAll()
+        {
+
+        }
+    }
+}
